36be4af54edfe1af3d2913f57222c5311686ea8b
Elisa Abedrapo <eabedrapo@gmail.com>
2019-07-05 15:49:56 -0500

    Connected Create Charter button with form

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', (req, res) => {
     res.render('index', {title: 'Explorer Tracker', brand: 'Explorer Tracker'});
 });
 
+router.get('/createCharter', (req, res) => {
+    res.render('newCharterForm', { title: 'Create a Charter', brand: 'Explorer Tracker'});
+});
+
 module.exports = router;
